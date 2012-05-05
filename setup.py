994dd877d46d350185b26803d14176ad5b06971c
Leon Handreke <leonh@ndreke.de>
2012-05-05 18:12:22 +0200

    Add setup.py for easy installation with pip

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='hpswitch',
+        version='0.1',
+        description="A library for interacting with HP Networking switches",
+        packages=['hpswitch', ],
+        url='https://github.com/leonhandreke/hpswitch',
+        license="MIT License",
+        )
